4165ece925fbe5a0146a34ab493ef1d6fb18b25e
Raul Rico <raulricovar@gmail.com>
2026-05-03 11:58:43 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   src/components/sections/contact-section.astro
    	modified:   src/components/sections/featured-projects.astro
    	modified:   src/components/sections/footer-section.astro
    	new file:   src/lib/constants/my_data.ts
    	new file:   src/pages/projects/1/index.astro

---
+++ src/lib/constants/my_data.ts
@@ -0,0 +1,4 @@
+export const MY_EMAIL = "raulricovar@gmail.com";
+export const MY_NAME = "Raul Rico";
+export const MY_LOCATION = "Mexico City, Mexico";
+export const MY_DESCRIPTION = "Full-stack developer building digital systems, platforms, tools and web3 applications for real-world use.";
